324fae5dbf36e6dea9c2b1b85c0506bb3d104951
fkaaziebu <frederickaziebu1998@gmail.com>
2023-04-19 16:35:18 +0000

    Added contact field for driver signup

--- server/__tests__/DriverRegister.spec.js
+++ server/__tests__/DriverRegister.spec.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const app = require("../app");
-// const Driver = require("../models/Driver");
+const Driver = require("../models/Driver");
 
 describe("Driver Registration", () => {
   it("returns 200 Ok when driver signup request is valid", (done) => {
@@ -9,6 +9,7 @@ describe("Driver Registration", () => {
       .send({
         username: "user1",
         email: "user1@mail.com",
+        contact: "0550815604",
         password: "P4ssword",
       })
       .then((response) => {
@@ -23,6 +24,7 @@ describe("Driver Registration", () => {
       .send({
         username: "user1",
         email: "user1@mail.com",
+        contact: "0550815604",
         password: "P4ssword",
       })
       .then((response) => {
@@ -31,18 +33,19 @@ describe("Driver Registration", () => {
       });
   });
 
-  // it("saves the user to database", (done) => {
-  //   request(app)
-  //     .post("/api/1.0/drivers/auth")
-  //     .send({
-  //       username: "user1",
-  //       email: "user1@mail.com",
-  //       password: "P4ssword",
-  //     })
-  //     .then((response) => {
-  //       const driversList = Driver.findAll();
-  //       expect(driversList.length).toBe(1);
-  //       done();
-  //     });
-  // });
+  it("saves the user to database", (done) => {
+    request(app)
+      .post("/api/1.0/drivers/auth")
+      .send({
+        username: "user1",
+        email: "user1@mail.com",
+        contact: "0550815604",
+        password: "P4ssword",
+      })
+      .then((response) => {
+        const driversList = Driver.findAll();
+        expect(driversList.length).toBe(1);
+        done();
+      });
+  });
 });
