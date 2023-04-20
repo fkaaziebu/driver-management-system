74c1eca6cdf0f01c4a8a732364e8ed767f38a1da
fkaaziebu <frederickaziebu1998@gmail.com>
2023-04-20 03:06:32 +0000

    Sign-up dev
    Developed the sign-up for frontend and set-up the backend to connect with the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
+import DriverLoginPage from "scenes/driverLoginPage";
 
 function App() {
-  return (
-    <div className="App">
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<DriverLoginPage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const driverAuthRoutes = require("./src/routes/driver/auth.js");
+const cors = require("cors");
 
 const app = express();
 
 /* MIDDLEWARE CONFIGS */
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors());
 
 /* ROUTES */
 app.use(driverAuthRoutes);
